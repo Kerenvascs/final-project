ae0118d9fa91a2f26b0c57fc9fdb33deaf265a52
kerenvasconcelos <kerenvasconcelos@gmail.com>
2020-07-21 22:24:57 +0100

    Added remove noise function.

---
+++ your-project/Cleaner functions.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import ast, json
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.tag import pos_tag
+from nltk.stem import WordNetLemmatizer
+# Import Punkt
+nltk.download('punkt')
